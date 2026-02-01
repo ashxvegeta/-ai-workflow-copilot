14076c26339193385cf3bd6e0ab767fb69e06866
ashxvegeta <ashutoshroy453@gmail.com>
2026-02-01 13:35:27 +0530

    Modify email_service.py to save data

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -1,12 +1,14 @@
-from app.services.ai_service import (
-    summarize_email,
-    extract_tasks,
-    detect_urgency
-)
+# Session = database transaction context
+# Passed from get_db() (dependency injection)
+from  sqlalchemy.orm import Session
+#ORM models: Email → parent table, Task → child table
+from app.database.models import Email, Task
+from app.routes import emails
+from app.services.ai_service import (summarize_email,extract_tasks,detect_urgency)
 
 
-def get_sample_emails():
-    emails = [
+def process_and_store_emails(db: Session):
+     emails = [
         {
             "from": "manager@company.com",
             "subject": "Project deadline",
@@ -18,9 +20,32 @@ def get_sample_emails():
             "body": "Hi team,Hope you're doing well.As discussed in yesterday’s meeting, please complete the dashboard by Friday.Let me know if you face blockers.Thanks,Manager"
         }
     ]
-    
-    for email in emails:
-        email["summary"] = summarize_email(email["body"])
-        email["tasks"] = extract_tasks(email["body"])
-        email["urgency"] = detect_urgency(email["body"])
-    return emails
+     
+     
+
+     saved_emails = []
+     for email in emails:
+        summary = summarize_email(email["body"])
+        tasks = extract_tasks(email["body"])
+        urgency = detect_urgency(email["body"])
+
+        email_obj = Email(
+            from_email=email["from"],
+            subject=email["subject"],
+            body=email["body"],
+            summary=summary,
+            urgency=urgency
+        )
+        db.add(email_obj)
+        db.commit()
+        db.refresh(email_obj)
+
+        for task_text in tasks:
+            task_obj = Task(
+                email_id=email_obj.id,
+                task_text=task_text
+            )
+            db.add(task_obj)
+        db.commit()
+        saved_emails.append(email_obj)
+        return saved_emails
