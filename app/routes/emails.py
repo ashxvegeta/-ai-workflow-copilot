4fe905804c6ddee3f51f3af67e5833d5b2243eca
ashxvegeta <ashutoshroy453@gmail.com>
2026-02-01 13:54:53 +0530

    Update route to use DB

--- app/routes/emails.py
+++ app/routes/emails.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
-from app.services.email_service import get_sample_emails
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database.db import get_db
+from app.services.email_service import process_and_store_emails
 
 router = APIRouter()
 
-@router.get("/sample")
-def fetch_sample_emails():
-    return get_sample_emails()
+@router.post("/process")
+def process_emails(db: Session = Depends(get_db)):
+    return process_and_store_emails(db)
